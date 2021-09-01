d186e9c11cb35e9d45a17302f9efd4f1fd25c694
TheDeathMuffin <samlongTGLM1@gmail.com>
2021-09-01 09:20:55 -0400

    Update samuel_long.php

--- samuel_long.php
+++ samuel_long.php
@@ -1,31 +1,35 @@
 <!doctype html>
 <html lang="en">
-	<?php
-		$classCode = "ASE 230";
-		$name = "Samuel Long";
-		$img = "images/profile.png";
-		$facebook = "https://www.facebook.com/";
-		$twitter = "https://www.twitter.com/";
-		$instragram = "https://www.instagram.com/";
-		$linkedin = "https://www.linkedin.com/";
-		$dream = "Web Developer/Backend-Programmer";
-		$dreamCo = "Tesla";
-		$email = "longs6@mymail.nku.edu";
-		$intro = "Living is changing. Unliving things do not change, at least not by their own will. So if you want to live, you must be prepared to let (or make) yourself change. It sounds easy to do, but can be very dificult because change often comes with discomfort and that is why learning to embrace that discomfort is so powerful.";
-		$quote = "Once someone decides to stop changing, they become stagnated in the past and the present passes them by.";
-		$funfact = "It is a general trend that the older one gets, the more set in their ways they become and consequently the less they are able to or are willing to accept and adhere to the new.";
-		$perc1 = "10%";
-		$perc2 = "85%";
-		$perc3 = "6%";
-	?>
-	<!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
-		
+	<head>
+		<?php
+			$classCode = "ASE 230";
+			$name = "Samuel Long";
+			$img = "images/profile.png";
+			$facebook = "https://www.facebook.com/";
+			$twitter = "https://www.twitter.com/";
+			$instragram = "https://www.instagram.com/";
+			$linkedin = "https://www.linkedin.com/";
+			$dream = "Web Developer/Backend-Programmer";
+			$dreamCo = "SpaceX";
+			$email = "longs6@mymail.nku.edu";
+			$intro = "Living is changing. Unliving things do not change, at least not by their own will. So if you want to live, you must be prepared to let (or make) yourself change. It sounds easy to do, but can be very dificult because change often comes with discomfort and that is why learning to embrace that discomfort is so powerful.";
+			$quote = "Once someone decides to stop changing, they become stagnated in the past and the present passes them by.";
+			$funfact = "It is a general trend that the older one gets, the more set in their ways they become and consequently the less they are able to or are willing to accept and adhere to the new.";
+			$skill1 = "Networking";
+			$skill2 = "System Administration";
+			$skill3 = "Programming";
+			$perc1 = "80%";
+			$perc2 = "70%";
+			$perc3 = "72%";
+		?>
+		<!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+		<title>This is <?php echo "$classCode - $name";?></title>
+	</head>
 	<body>
 		<link rel="stylesheet" href="assets/css/detail.css" />
-		<title>ASE 230 - <?php echo 'Samuel Long'; ?></title>
 		<div class="container text-center mb-5">
 			<h1>This is ASE 230 - <?php echo 'Samuel Long'; ?></h1>
 		</div>
@@ -82,7 +86,7 @@
 					<div class="py-1">
 						<div class="progress">
 							<div class="progress-bar" role="progressbar" style="width: 10%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
-								<div class="progress-bar-title">Finance</div>
+								<div class="progress-bar-title"><?php echo $skill1;?></div>
 								<span class="progress-bar-number"><?php echo $perc1;?></span>
 							</div>
 						</div>
@@ -90,7 +94,7 @@
 					<div class="py-1">
 						<div class="progress">
 							<div class="progress-bar" role="progressbar" style="width:85%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
-								<div class="progress-bar-title">Information Technologies</div>
+								<div class="progress-bar-title"><?php echo $skill2;?></div>
 								<span class="progress-bar-number"><?php echo $perc2;?></span>
 							</div>
 						</div>
@@ -98,7 +102,7 @@
 					<div class="py-1">
 						<div class="progress">
 							<div class="progress-bar" role="progressbar" style="width:6%" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100">
-								<div class="progress-bar-title">Education</div>
+								<div class="progress-bar-title"><?php echo $skill3;?></div>
 								<span class="progress-bar-number"><?php echo $perc3;?></span>
 							</div>
 						</div>
