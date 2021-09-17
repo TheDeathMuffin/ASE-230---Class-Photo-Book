b1fbea648c1a2ef04a0e29b84e19622e5fb286ca
TheDeathMuffin <samlongTGLM1@gmail.com>
2021-09-16 22:02:19 -0400

    Applied new template with an expanded data set.
    Implemented a new (terrible) template using bootstrap. Data.php now includes data from several class members.

--- detail.php
+++ detail.php
@@ -7,93 +7,72 @@
 		[$ageYear, $ageMonth, $ageDay] = calculateAge($masterArray[$i]["DOB"]);				#Date of birth is calculated and returned into the values #ageYear, $ageMonth, and $ageDay. 
 	?>
 	<head>
-		<!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+		<title>This is ASE 230 - <?=$masterArray[$i]["name"]?></title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
-		<title>This is <?="$classCode?"?> - <?=$masterArray[$i]["name"]?></title>
 	</head>
-	<body>
-		<link rel="stylesheet" href="assets/css/detail.css" />
-		<div class="container text-center mb-5">
-			<h1>This is <?= "$classCode"?> - <?=$masterArray[$i]["name"]?></h1>
+	<body style="background-color: #98B6B1;">		
+		<div class="container-fluid text-center" style="padding: 10px; margin-top: 30px auto; height: auto; background-color: #D7BCC8;">
+			<h1>This is ASE 230 - <?=$masterArray[$i]["name"]?></h1>
 		</div>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-5 col-md-6">
-					<div class="mb-2">
-						<img class="w-100" src="<?=$masterArray[$i]["img"]?>" alt="">
-					</div>
-					<div class="mb-2 d-flex">
-						<h4 class="font-weight-normal"><?=$masterArray[$i]["name"]?></h4>
-						<div class="social d-flex ml-auto">
-							<p class="pr-2 font-weight-normal">Follow on:</p>
-							<a href="<?=$masterArray[$i]["socialMedia"]?>" class="text-muted mr-1">
-								<i class="fab fa-facebook-f"></i>
-							</a>
-							<a href="<?=$masterArray[$i]["socialMedia"]?>" class="text-muted mr-1">
-								<i class="fab fa-twitter"></i>
-							</a>
-							<a href="<?=$masterArray[$i]["socialMedia"]?>" class="text-muted mr-1">
-								<i class="fab fa-instagram"></i>
-							</a>
-							<a href="<?=$masterArray[$i]["socialMedia"]?>" class="text-muted">
-								<i class="fab fa-linkedin"></i>
-							</a>
-						</div>
-					</div>
-					<div class="mb-2">
-					<ul class="list-unstyled">
-					<li class="media">
-					<span class="w-25 text-black font-weight-normal">Dream profession:</span>
-					<label class="media-body"><?=$masterArray[$i]["dProfession"]?></label>
-					</li>
-					<li class="media">
-					<span class="w-25 text-black font-weight-normal">Dream company: </span>
-					<label class="media-body"><?=$masterArray[$i]["dCompany"]?></label>
-					</li>
-					<li class="media">
-					<span class="w-25 text-black font-weight-normal">Email: </span>
-					<label class="media-body"><?=$masterArray[$i]["email"]?></label>
-					</li>
-					</ul>
+		<div class="container-fluid" style="padding-top: 25px; margin-bottom: 25px;">
+<!--#image on the right, details on the left. intro stuff below. Or intro on the lft. -->
+			<div class="row" style="height: 0 auto;">
+				
+				<div class="col-md-4 col-sm-12 col-xs-12">
+					<div class="rounded-top" style="background-color: #629677; text-align: center; padding-top: 18px;"><img src="<?=$masterArray[$i]["img"]?>" class="img-fluid" style="padding: 10px;"></div>
+				</div>
+				<div class="col-md-3 col-sm-12 col-xs-12" style="">
+					<div class="rounded-right" style="background-color: #629677; padding: 10px; margin-left: -40px; height: 92%;">
+						<h3><?=$masterArray[$i]["name"]?></h3>
+						<h6>Age: <?= $ageYear?></h6>
+						<p><i><?= $masterArray[$i]["name"]?> was born <?= "$ageYear years, $ageMonth months, and $ageDay days ago."?></i></p>
+						<p>Social Media:</p>
+						<h6><a href="<?=$masterArray[$i]["linkedIn"]?>">LinkedIn</a></h6>
+						<h6><a href="<?=$masterArray[$i]["twitter"]?>">Twitter</a></h6>
+						<h6><a href="<?=$masterArray[$i]["facebook"]?>">Facebook</a></h6>
+						<h6><a href="<?=$masterArray[$i]["instagram"]?>">Instrgram</a></h6>
 					</div>
 				</div>
-				<div class="col-lg-7 col-md-6 pl-xl-3">
-					<h5 class="font-weight-normal">Short intro</h5>
-					<p><?=$masterArray[$i]["intro"]?></p>
-					<h6>Age: <?= $ageYear?></h6>
-					<h7><i><?= $masterArray[$i]["name"]?> was born <?= "$ageYear years, $ageMonth months, and $ageDay days ago."?></i></h7>
-					<div class="my-2 bg-light p-2">
-						<p class="font-italic mb-0"><?=$masterArray[$i]["quote"]?></p>
+				<div class="col-md-3 col-sm-12 col-xs-12"">
+					<div class="rounded" style="background-color: #629677; padding: 15px; height: 92%;">
+						<p>Dream profession: <br><?=$masterArray[$i]["dProfession"]?></p>
+						<p>Email: <br><?=$masterArray[$i]["email"]?></p>
+						<p>Dream company: <br><?=$masterArray[$i]["dCompany"]?></p>
 					</div>
-					<div class="mb-2 mt-2 pt-1">
-						<h5 class="font-weight-normal">Top skills</h5>
+				</div>
+				
+				<div class="col-md-2 col-sm-12 col-xs-12">
+					<div class="rounded" style="background-color: #629677; padding: 15px; height: 92%;">
+						<?php
+						foreach($masterArray[$i]["skills"] as $skill) {
+						?>
+						<p><?=$skill["name"],": "?></p>
+						<h6><i><?=$skill["value"],"/100"?></i></h6>
+						<?php }	?>
 					</div>
-					
-					
-					<?php
-					foreach($masterArray[$i]["skills"] as $skill) {
-					?>
-					<div class="py-1">
-						<div class="progress">
-							<div class="progress-bar" role="progressbar" style="width: <?=$skill["value"]?>%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
-								<div class="progress-bar-title"><?=$skill["name"]?></div>
-								<span class="progress-bar-number"><?=$skill["value"]?></span>
-							</div>
-						</div>
+				</div>
+			
+			</div>
+			<div class="row" style="height: 200px;">
+				<div class="col-md-4 col-sm-12 col-xs-12">
+					<div class="rounded-bottom" style="background-color: #629677; padding: 10px;">
+						<h5 class="font-weight-normal">Short intro</h5>
+						<p><?=$masterArray[$i]["intro"]?></p>
 					</div>
-					<?php }	?>
-					
-					
-					<h5 class="font-weight-normal">Fun fact</h5>
-					<p><?=$masterArray[$i]["funfact"]?></p>
-					<a href="<?='index.php';?>" class="font-weight-normal">
-						<u><center><?='Back to Homepage';?></center></u>
-					</a>
+				</div>
+				<div class="col-md-8 col-sm-12 col-xs-12">
+					<div class="rounded" style="padding: 15px; background-color: #629677;">
+						<h5 class="font-weight-normal">Quote</h5>
+						<p><?=$masterArray[$i]["quote"]?></p>
+						<h5 class="font-weight-normal">Fun Fact</h5>
+						<p><?=$masterArray[$i]["funfact"]?></p>
+					</div>
 				</div>
 			</div>
 		</div>
+		<center><a href="index.php">Back to Homepage</a></center><br>
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 	</body>
--- index.php
+++ index.php
@@ -1,73 +1,79 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="en">
 	<?php 			
 		require('data.php');				#Loads data.php into file so that student data can be accessed.
 		require('functions.php');			#Loads functions.php into file so that the calculateAge() function can be used.
+		
 		function cardDisplay($Index) {			#This function displays a student's card with their corresponding information given their ID as a parameter.
 			require('data.php');			#Loads data.php into file so that student data can be accessed within the function.
 			[$ageYear, $ageMonth, $ageDay] = calculateAge($masterArray[$Index]["DOB"]);	#Date of birth is calculated and returned into the values #ageYear, $ageMonth, and $ageDay.
-	?>										<!-- Card is created using HTML for structuring with PHP inserting student data. -->
-			<div class="col-12 col-sm-6 col-lg-3">
-				<div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
-					<!-- Team Thumb-->
-					<div class="advisor_thumb"><a href="detail.php?Index=<?=$Index?>"><img src="<?=$masterArray[$Index]["img"];?>" alt=""></a>
-						<!-- Social Info-->
-						<div class="social-info"><a href="<?=$masterArray[$Index]["socialMedia"];?>"><i class="fa fa-facebook"></i></a><a href="<?=$masterArray[$Index]["socialMedia"];?>"><i class="fa fa-twitter"></i></a><a href="<?=$masterArray[$Index]["socialMedia"];?>"><i class="fa fa-linkedin"></i></a></div>
-					</div>
-					<!-- Team Details-->
-					<?php
-					for($t=0;$t<$masterArray[$Index]["year"];$t++) {
-					?>
-						<img src="images/year.png" alt="">
-					<?php } ?>
-					<div class="single_advisor_details_info">
-						<h6><?=$masterArray[$Index]["name"];?></h6>
-						<p class="designation"><?=$masterArray[$Index]["job"];?></p>
-						<h7><?= "Age: $ageYear";?></h7><br>
-						<p><i><?= "Time since DOB: $ageYear years, $ageMonth months, and $ageDay days.";?></i></p>
-					</div>
-				</div>
-			</div>
-		<?php
+			switch ($masterArray[$Index]["year"]){
+				case 1: $textYear = "Freshman";
+					break;
+				case 2: $textYear = "Sophomore";
+					break;
+				case 3: $textYear = "Junior";
+					break;
+				case 4: $textYear = "Senior";
+					break;
+				default:
+					$textYear = "Undefined";
 			}
-		?>
-	
-	<head>
-	<!-- https://www.bootdey.com/snippets/view/single-advisor-profile#html -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-	<link rel="stylesheet" href="assets/css/index.css" />
-	<title><?= $classCode;?> - class of <?= "$classSeason $classYear";?></title>		
-	</head>
-	
-	<body>
-		<div class="container text-center">
-			<h1>This is <?= $classCode;?> - class of <?= $classSeason; ?> <?= $classYear ?></h1>
-		</div>
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-12 col-sm-8 col-lg-6">
-					<!-- Section Heading-->
-					<div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
-						<h3>Our Creative <span> Team</span></h3>
-						<p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
-						<div class="line"></div>
+	?>										<!-- Card is created using HTML for structuring with PHP inserting student data. -->
+			<div class="col-md-4 col-sm-6 col-xs-12 content" style="">
+				<div class="rounded" style="background-color: #629677; height: auto; margin-top: 25px; margin-bottom: 30px; padding: 15px;">
+					<a href="detail.php?Index=<?=$Index?>"><img src="<?=$masterArray[$Index]["img"];?>" alt="profile image" class="img-fluid"></a>
+					<div class="row">
+						<div class="col-md-8 col-sm-1 col-xs-1 content">
+							<div style="text-align: left;">
+								<h3><?=$masterArray[$Index]["name"];?></h3>
+								<i>Role: <?=$masterArray[$Index]['job'];?></i><br>
+								<i>Year: <?=$textYear;?></i><br>
+								<i>Age: <?=$ageYear;?></i><br>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-1 col-xs-1 content">
+							<div style="text-align: right;">
+								<br><u>Social Media</u><br>
+								<a href="<?=$masterArray[$Index]['linkedIn'];?>">LinkedIn</a><br>
+								<a href="<?=$masterArray[$Index]['facebook'];?>">Facebook</a><br>
+								<a href="<?=$masterArray[$Index]['twitter'];?>">Twitter</a>
+							</div>
+						</div>
 					</div>
 				</div>
+			</div>	
+	<?php } ?>
+	<head>
+		<title>This is ASE 230</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	</head>
+	<body>	
+		<div style="max-width: 1300px; margin: 0 auto; padding: 0px;">
+			
+			<div class="container-fluid text-center" style="padding: 10px; margin-top: 30px auto; height: auto; background-color: #D7BCC8;">
+				<h1>This is ASE 230 - class of Fall 2021</h1>
+				<h2>Our Creative Team</h2>
 			</div>
-			<div class="row">
-					
+		
+			
+			
+			<div class="row text-center" style="background-color: #98B6B1;">
 				<?php
-					for ($x = 0; $x < count($masterArray); $x++) {		#Loops for as many arrays that are contained within $masterArray. 
-						cardDisplay($x);				#For each student, their $masterArray index is passed in as an argument for the function cardDisplay(), which then displays a card containing the student's information.
+					for ($x = 0; $x < count($masterArray); $x++) {
+						cardDisplay($x);
 					}
 				?>
-
 			</div>
-		</div>		
+		
+		
+		
+		</div>
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 	</body>
 </html>
+
+
+<div class=""></div>
