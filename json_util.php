9ebc55de9a44d476dc2f34fb06e40e8bac40b549
TheDeathMuffin <samlongTGLM1@gmail.com>
2021-09-22 18:08:23 -0400

    Update json_util.php

--- json_util.php
+++ json_util.php
@@ -1,10 +1,10 @@
 <?php
-	
+#readJSON effectively reads a json file and formats the information into an associative array. 
 function readJSON($json){
 	$file = json_decode(file_get_contents($json), true);
 	return $file;
 }
-
+#saveJSON effectively stores the information from an array into a json file. 
 function saveJSON($arrayToSave, $json){
 	file_put_contents($json,json_encode($arrayToSave, JSON_PRETTY_PRINT));
 }
